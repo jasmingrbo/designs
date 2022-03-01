d2c19bcc36dee00b3044b62098e225535dd8fb61
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2022-03-01 17:31:17 +0100

    feat: add shined corner on categories

--- doctors/src/main/java/ba/grbo/doctors/composables/Categories.kt
+++ doctors/src/main/java/ba/grbo/doctors/composables/Categories.kt
@@ -2,11 +2,16 @@ package ba.grbo.doctors.composables
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
@@ -16,6 +21,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -31,14 +37,22 @@ import ba.grbo.doctors.Category.PHYSICIAN
 import ba.grbo.doctors.Category.SKIN
 import ba.grbo.doctors.Category.SURGEON
 import ba.grbo.doctors.R
+import ba.grbo.doctors.ui.theme.bitterSweet
+import ba.grbo.doctors.ui.theme.brightTurquoise
 import ba.grbo.doctors.ui.theme.coral
 import ba.grbo.doctors.ui.theme.dodgerBlue
 import ba.grbo.doctors.ui.theme.heliotrope
+import ba.grbo.doctors.ui.theme.hotPink
+import ba.grbo.doctors.ui.theme.malibu
+import ba.grbo.doctors.ui.theme.melrose
+import ba.grbo.doctors.ui.theme.mountainMeadow
 import ba.grbo.doctors.ui.theme.razzleDazzleRose
 import ba.grbo.doctors.ui.theme.robinsEggBlue
 import ba.grbo.doctors.ui.theme.seaBuckthorn
 import ba.grbo.doctors.ui.theme.sunsetOrange
+import ba.grbo.doctors.ui.theme.vividTangerine
 import ba.grbo.doctors.ui.theme.white
+import ba.grbo.doctors.ui.theme.yellowOrange
 
 @Composable
 fun Categories(
@@ -54,6 +68,7 @@ fun Categories(
         ) {
             Category(
                 boxColor = MaterialTheme.colors.primary,
+                cornerShineColor = malibu,
                 iconResource = R.drawable.ic_stethoscope,
                 textResource = R.string.doctors_consultation,
                 onClicked = { onCategoryButtonClicked(CONSULTATION) }
@@ -61,6 +76,7 @@ fun Categories(
             HorizontalSpacer(27.dp)
             Category(
                 boxColor = heliotrope,
+                cornerShineColor = melrose,
                 iconResource = R.drawable.ic_tooth,
                 textResource = R.string.doctors_dental,
                 onClicked = { onCategoryButtonClicked(DENTAL) }
@@ -68,6 +84,7 @@ fun Categories(
             HorizontalSpacer(32.dp)
             Category(
                 boxColor = coral,
+                cornerShineColor = vividTangerine,
                 iconResource = R.drawable.ic_heart,
                 textResource = R.string.doctors_heart,
                 onClicked = { onCategoryButtonClicked(HEART) }
@@ -75,6 +92,7 @@ fun Categories(
             HorizontalSpacer(32.dp)
             Category(
                 boxColor = seaBuckthorn,
+                cornerShineColor = yellowOrange,
                 iconResource = R.drawable.ic_clinic,
                 textResource = R.string.doctors_hospitals,
                 onClicked = { onCategoryButtonClicked(HOSPITALS) }
@@ -89,24 +107,28 @@ fun Categories(
         ) {
             Category(
                 boxColor = MaterialTheme.colors.secondary,
+                cornerShineColor = mountainMeadow,
                 iconResource = R.drawable.ic_medicine,
                 textResource = R.string.doctors_medicines,
                 onClicked = { onCategoryButtonClicked(MEDICINES) }
             )
             Category(
                 boxColor = robinsEggBlue,
+                cornerShineColor = brightTurquoise,
                 iconResource = R.drawable.ic_care,
                 textResource = R.string.doctors_physician,
                 onClicked = { onCategoryButtonClicked(PHYSICIAN) }
             )
             Category(
                 boxColor = razzleDazzleRose,
+                cornerShineColor = hotPink,
                 iconResource = R.drawable.ic_bandage,
                 textResource = R.string.doctors_skin,
                 onClicked = { onCategoryButtonClicked(SKIN) }
             )
             Category(
                 boxColor = sunsetOrange,
+                cornerShineColor = bitterSweet,
                 iconResource = R.drawable.ic_syringe,
                 textResource = R.string.doctors_surgeon,
                 onClicked = { onCategoryButtonClicked(SURGEON) }
@@ -119,6 +141,7 @@ fun Categories(
 private fun Category(
     modifier: Modifier = Modifier,
     boxColor: Color,
+    cornerShineColor: Color,
     @DrawableRes iconResource: Int,
     @StringRes textResource: Int,
     onClicked: () -> Unit
@@ -131,13 +154,24 @@ private fun Category(
             modifier = Modifier.size(56.dp),
             shape = MaterialTheme.shapes.medium,
             colors = ButtonDefaults.buttonColors(backgroundColor = boxColor, contentColor = white),
+            contentPadding = PaddingValues(0.dp),
             onClick = onClicked
         ) {
-            Icon(
-                modifier = Modifier.align(Alignment.CenterVertically),
-                painter = painterResource(iconResource),
-                contentDescription = null
-            )
+            Box(modifier = Modifier.fillMaxSize()) {
+                Canvas(modifier = Modifier.requiredSize(20.dp)) {
+                    drawCircle(
+                        color = cornerShineColor,
+                        center = Offset(x = 0f, y = 0f),
+                        radius = size.maxDimension
+                    )
+                }
+
+                Icon(
+                    modifier = Modifier.align(Alignment.Center),
+                    painter = painterResource(iconResource),
+                    contentDescription = null
+                )
+            }
         }
         VerticalSpacer(8.dp)
         Text(
@@ -153,6 +187,7 @@ fun PreviewCategory() {
     Preview {
         Category(
             boxColor = dodgerBlue,
+            cornerShineColor = malibu,
             iconResource = R.drawable.ic_stethoscope,
             textResource = R.string.doctors_consultation,
             onClicked = {}
