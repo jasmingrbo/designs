41cd04461e1548bf1089ae1b78a7d7b831c088f0
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2022-03-05 11:11:16 +0100

    feat(job finder): add colors and fonts

--- jobfinder/src/main/java/ba/grbo/jobfinder/ui/theme/Color.kt
+++ jobfinder/src/main/java/ba/grbo/jobfinder/ui/theme/Color.kt
@@ -2,7 +2,10 @@ package ba.grbo.jobfinder.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+val mineShaft = Color(0xFF333333)
+val alto = Color(0xFFE0E0E0)
+val eastBay = Color(0xFF3E4F88)
+val wildSand = Color(0xFFF6F6F6)
