7975e3109435b578c6d03a7a041a280ed6e0a514
Jasmin Grbo <46293362+grb0@users.noreply.github.com>
2022-03-05 13:50:05 +0100

    chore(doctors): change user image click implementation

--- doctors/src/main/java/ba/grbo/doctors/composables/destinations/DoctorsScreen.kt
+++ doctors/src/main/java/ba/grbo/doctors/composables/destinations/DoctorsScreen.kt
@@ -1,6 +1,8 @@
 package ba.grbo.doctors.composables.destinations
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -12,11 +14,13 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextButton
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
@@ -24,6 +28,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
@@ -66,13 +71,18 @@ fun DoctorsScreen(
                 }
             },
             trailingIcon = {
-                IconButton(onClick = onUserButtonClicked) {
-                    Image(
-                        modifier = Modifier.size(36.dp),
-                        painter = painterResource(R.drawable.user),
-                        contentDescription = stringResource(R.string.doctors_user)
-                    )
-                }
+                Image(
+                    modifier = Modifier
+                        .size(36.dp)
+                        .clip(CircleShape)
+                        .clickable(
+                            onClick = onUserButtonClicked,
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = rememberRipple()
+                        ),
+                    painter = painterResource(R.drawable.user),
+                    contentDescription = stringResource(R.string.doctors_user)
+                )
             }
         )
         VerticalSpacer(24.dp)
